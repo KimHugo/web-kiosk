221037082a9d48e1b08c9381828abd8c0c61f785
KimHugo <hugobayit@gmail.com>
2023-10-15 12:46:13 +0900

    fix]사이드바 넓이 확대(10%->20%)

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -4,7 +4,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function Root() {
   return (
     <div className="flex w-full h-full">
-      <aside className="grid-rows-4 grid h-full w-[10%] items-center">
+      <aside className="grid-rows-4 grid h-full w-[20%] items-center">
         <img src="" />
         <Link to={"/1f"}>1F</Link>
         <Link to={"/2f"}>2F</Link>
