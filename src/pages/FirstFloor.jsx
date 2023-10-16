ca028d99251acd31bb910c6b6faba410ed3c5d27
KimHugo <hugobayit@gmail.com>
2023-10-16 11:28:30 +0900

    img] 1f 배경 이미지 추가

--- src/pages/FirstFloor.jsx
+++ src/pages/FirstFloor.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function FirstFloor() {
   return (
-    <>
+    <div className="bg-contain bg-no-repeat bg-[url('./imgs/1f.png')] w-full">
       Hello, First Floor!
       <div>
         <Link to={"/room/1fRoom1"}>
@@ -17,6 +17,6 @@ export default function FirstFloor() {
           <MdOutlineRoom />
         </Link>
       </div>
-    </>
+    </div>
   );
 }
