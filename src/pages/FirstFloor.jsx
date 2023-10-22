675ebbbfcb1a78ef12487285ffce5081b0e80d02
KimHugo <hugobayit@gmail.com>
2023-10-23 00:26:42 +0900

    fix]1F 마커 사이즈 px->vw로 변경

--- src/pages/FirstFloor.jsx
+++ src/pages/FirstFloor.jsx
@@ -7,9 +7,9 @@ export default function FirstFloor() {
     <div  className="bg-[#FF0000] w-[80%] flex justify-center items-center">
       <div className="bg-contain bg-no-repeat bg-[url('./imgs/1f.png')] w-full h-full">
         <div className="relative w-full h-full">
-          <Link className="fixed top-[29vw] left-[77vw] right-0" to={"/room/1F_VS"}>
+          <Link className="fixed top-[29vw] left-[76.9vw] right-0" to={"/room/1F_VS"}>
           {/* <Link className="fixed top-[47%] left-[77%] right-0" to={"/room/1F_VS"}> */}
-            <MdOutlineRoom className="w-[50px] h-[50px]" />
+            <MdOutlineRoom className="w-[3.5vw] h-[3.5vw]" />
           </Link>
           {/* <Link to={"/room/1fRoom2"}>
             <MdOutlineRoom />
--- src/pages/SecondFloor.jsx
+++ src/pages/SecondFloor.jsx
@@ -8,19 +8,19 @@ export default function SecondFloor() {
       <div className="bg-contain bg-no-repeat bg-[url('./imgs/2f.png')] w-full h-full">
         <div className="relative w-full h-full">
           <Link
-            className="fixed top-[400px] left-[720px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[28vw] left-[720px] right-0 w-[70px] h-[70px]"
             to={"/room/2f_S201"}
           >
             <MdOutlineRoom className="w-[50px] h-[50px]" />
           </Link>
           <Link
-            className="fixed top-[400px] left-[450px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[28vw] left-[450px] right-0 w-[70px] h-[70px]"
             to={"/room/2f_S204"}
           >
             <MdOutlineRoom className="w-[50px] h-[50px]" />
           </Link>
           <Link
-            className="fixed top-[635px] left-[542px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[44vw] left-[542px] right-0 w-[70px] h-[70px]"
             to={"/room/2f_S206"}
           >
             <MdOutlineRoom className="w-[50px] h-[50px]" />
