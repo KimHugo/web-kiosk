985bce4bd502c9ffe78035185a3b3474eba09103
KimHugo <hugobayit@gmail.com>
2023-10-23 00:37:26 +0900

    2F, 3F 마커 위치 및 크기 단위 px->vw로 변경

--- src/pages/SecondFloor.jsx
+++ src/pages/SecondFloor.jsx
@@ -8,22 +8,22 @@ export default function SecondFloor() {
       <div className="bg-contain bg-no-repeat bg-[url('./imgs/2f.png')] w-full h-full">
         <div className="relative w-full h-full">
           <Link
-            className="fixed top-[28vw] left-[720px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[28vw] left-[50vw] right-0 w-[70px] h-[70px]"
             to={"/room/2f_S201"}
           >
-            <MdOutlineRoom className="w-[50px] h-[50px]" />
+            <MdOutlineRoom className="w-[3.5vw] h-[3.5vw]" />
           </Link>
           <Link
-            className="fixed top-[28vw] left-[450px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[28vw] left-[31.7vw] right-0 w-[70px] h-[70px]"
             to={"/room/2f_S204"}
           >
-            <MdOutlineRoom className="w-[50px] h-[50px]" />
+            <MdOutlineRoom className="w-[3.5vw] h-[3.5vw]" />
           </Link>
           <Link
-            className="fixed top-[44vw] left-[542px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[44vw] left-[38vw] right-0 w-[70px] h-[70px]"
             to={"/room/2f_S206"}
           >
-            <MdOutlineRoom className="w-[50px] h-[50px]" />
+            <MdOutlineRoom className="w-[3.5vw] h-[3.5vw]" />
           </Link>
         </div>
       </div>
--- src/pages/ThirdFloor.jsx
+++ src/pages/ThirdFloor.jsx
@@ -8,28 +8,28 @@ export default function ThirdFloor() {
       <div className="bg-contain bg-no-repeat bg-[url('./imgs/3f.png')] w-full h-full">
         <div className="relative w-full h-full">
           <Link
-            className="fixed top-[400px] left-[720px] right-0 w-[70px] h-[70px]]"
+            className="fixed top-[28vw] left-[50vw] right-0 w-[70px] h-[70px]]"
             to={"/room/3f_S301"}
           >
-            <MdOutlineRoom className="w-[50px] h-[50px]" />
+            <MdOutlineRoom className="w-[3.5vw] h-[3.5vw]" />
           </Link>
           <Link
-            className="fixed top-[400px] left-[545px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[28vw] left-[38vw] right-0 w-[70px] h-[70px]"
             to={"/room/3f_S303"}
           >
-            <MdOutlineRoom className="w-[50px] h-[50px] " />
+            <MdOutlineRoom className="w-[3.5vw] h-[3.5vw]" />
           </Link>
           <Link
-            className="fixed top-[410px] left-[1155px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[29vw] left-[80.5vw] right-0 w-[70px] h-[70px]"
             to={"/room/3f_M301"}
           >
-            <MdOutlineRoom className="w-[50px] h-[50px]" />
+            <MdOutlineRoom className="w-[3.5vw] h-[3.5vw]" />
           </Link>
           <Link
-            className="fixed top-[550px] left-[1200px] right-0 w-[70px] h-[70px]"
+            className="fixed top-[37.5vw] left-[83.3vw] right-0 w-[70px] h-[70px]"
             to={"/room/3f_M302"}
           >
-            <MdOutlineRoom className="w-[50px] h-[50px]" />
+            <MdOutlineRoom className="w-[3.5vw] h-[3.5vw]" />
           </Link>
         </div>
       </div>
