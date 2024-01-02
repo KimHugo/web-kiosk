381d9ba78620fccb9bd436b394c264181174360b
poeticDev <poeticdev23@gmail.com>
2024-01-02 11:16:12 +0900

    Fix:

--- src/components/RoomComponent.jsx
+++ src/components/RoomComponent.jsx
@@ -1,51 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { AiOutlineRollback } from "react-icons/ai";
-import { useNavigate, useParams } from "react-router-dom";
+//import { AiOutlineRollback } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+//import { useNavigate, useParams } from "react-router-dom";
 
 export default function RoomComponent() {
   const param = useParams().roomId;
   const [roomNum, setRoomNum] = useState("");
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
 
-  const handleBackUrl = () => {
-    if (param === "1F_VS") {
-      window.location.pathname = "1f";
-    } else if (param === "2f_S201") {
-      window.location.pathname = "2f";
-    } else if (param === "2f_S204") {
-      window.location.pathname = "2f";
-    } else if (param === "2f_S206") {
-      window.location.pathname = "2f";
-    } else if (param === "3f_S301") {
-      window.location.pathname = "3f";
-    } else if (param === "3f_S303") {
-      window.location.pathname = "3f";
-    } else if (param === "3f_M301") {
-      window.location.pathname = "3f";
-    } else if (param === "3f_M302") {
-      window.location.pathname = "3f";
-    }
-  };
+  // const handleBackUrl = () => {
+  //   if (param === "1F_VS") {
+  //     window.location.pathname = "1f";
+  //   } else if (param === "2f_S201") {
+  //     window.location.pathname = "2f";
+  //   } else if (param === "2f_S204") {
+  //     window.location.pathname = "2f";
+  //   } else if (param === "2f_S206") {
+  //     window.location.pathname = "2f";
+  //   } else if (param === "3f_S301") {
+  //     window.location.pathname = "3f";
+  //   } else if (param === "3f_S303") {
+  //     window.location.pathname = "3f";
+  //   } else if (param === "3f_M301") {
+  //     window.location.pathname = "3f";
+  //   } else if (param === "3f_M302") {
+  //     window.location.pathname = "3f";
+  //   }
+  // };
 
-  let QRImgUrl;
+  // let QRImgUrl;
 
-  if (param === "1F_VS") {
-    QRImgUrl = "/imgs/QR/1F_VS.png";
-  } else if (param === "2f_S201") {
-    QRImgUrl = "/imgs/QR/2f_S201.png";
-  } else if (param === "2f_S204") {
-    QRImgUrl = "/imgs/QR/2f_S204.png";
-  } else if (param === "2f_S206") {
-    QRImgUrl = "/imgs/QR/2f_S206.png";
-  } else if (param === "3f_S301") {
-    QRImgUrl = "/imgs/QR/3f_S301.png";
-  } else if (param === "3f_S303") {
-    QRImgUrl = "/imgs/QR/3f_S303.png";
-  } else if (param === "3f_M301") {
-    QRImgUrl = "/imgs/QR/3f_M301.png";
-  } else if (param === "3f_M302") {
-    QRImgUrl = "/imgs/QR/3f_M302.png";
-  }
+  // if (param === "1F_VS") {
+  //   QRImgUrl = "/imgs/QR/1F_VS.png";
+  // } else if (param === "2f_S201") {
+  //   QRImgUrl = "/imgs/QR/2f_S201.png";
+  // } else if (param === "2f_S204") {
+  //   QRImgUrl = "/imgs/QR/2f_S204.png";
+  // } else if (param === "2f_S206") {
+  //   QRImgUrl = "/imgs/QR/2f_S206.png";
+  // } else if (param === "3f_S301") {
+  //   QRImgUrl = "/imgs/QR/3f_S301.png";
+  // } else if (param === "3f_S303") {
+  //   QRImgUrl = "/imgs/QR/3f_S303.png";
+  // } else if (param === "3f_M301") {
+  //   QRImgUrl = "/imgs/QR/3f_M301.png";
+  // } else if (param === "3f_M302") {
+  //   QRImgUrl = "/imgs/QR/3f_M302.png";
+  // }
 
   useEffect(() => {
     if (param === "1F_VS") {
@@ -85,7 +86,11 @@ export default function RoomComponent() {
 
   return (
     <div className="w-[80%] h-full relative">
-      <iframe className="w-full h-full z-[8]" src={roomNum}></iframe>
+      <iframe
+        title="roomNum"
+        className="w-full h-full z-[8]"
+        src={roomNum}
+      ></iframe>
       <div>
         {/* <button className="absolute bottom-4 left-4 z-10 bg-white text-4xl">
           <img className="w-[80px] h-[80px]" src={QRImgUrl} alt="QR CODE" />
